063273e253bfeb95c551d75d42ee2840aacfb2d7
Christian Schrefl <chrisi.schrefl@gmail.com>
2024-06-06 17:22:35 +0200

    arm: rust: Enable Rust support for ARMv7
    This commit allows building ARMv7 kernels with Rust support.
    
    The rust core library expects some __eabi_... functions
    that are not implemented in the kernel.
    Those functions are some float operations and __aeabi_uldivmod.
    For now those are implemented with define_panicking_intrinsics!.
    
    This is based on the code by Sven Van Asbroeck from the original
    rust branch and inspired by the AArch version by Jamie Cunliffe.
    
    I have tested the rust samples and a custom simple MMIO module
    on hardware (De1SoC FPGA + Arm A9 CPU).
    
    Tested-by: Rudraksha Gupta <guptarud@gmail.com>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Acked-by: Miguel Ojeda <ojeda@kernel.org>
    Tested-by: Miguel Ojeda <ojeda@kernel.org>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Christian Schrefl <chrisi.schrefl@gmail.com>

--- rust/compiler_builtins.rs
+++ rust/compiler_builtins.rs
@@ -73,5 +73,21 @@ define_panicking_intrinsics!("`u128` should not be used", {
     __umodti3,
 });
 
+#[cfg(target_arch = "arm")]
+define_panicking_intrinsics!("`f32` should not be used", {
+    __aeabi_fcmpeq,
+    __aeabi_fcmpun,
+});
+
+#[cfg(target_arch = "arm")]
+define_panicking_intrinsics!("`f64` should not be used", {
+    __aeabi_dcmpun,
+});
+
+#[cfg(target_arch = "arm")]
+define_panicking_intrinsics!("`u64` division/modulo should not be used", {
+    __aeabi_uldivmod,
+});
+
 // NOTE: if you are adding a new intrinsic here, you should also add it to
 // `redirect-intrinsics` in `rust/Makefile`.
