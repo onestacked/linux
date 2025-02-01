67574fa90533839b65b26598cdc35aa58c3c55a4
Christian Schrefl <chrisi.schrefl@gmail.com>
2025-02-01 01:12:52 +0100

    rust: fix firmware abstraction for 32-bit arm
    Fixes a small build failure with rust firmware abstraction and 32-bit
    arm where a mismatch between u8 and i8 happens because of different char
    signednes.
    
    Fixes: de6582833db0 ("rust: add firmware abstractions")
    Signed-off-by: Christian Schrefl <chrisi.schrefl@gmail.com>

--- rust/kernel/firmware.rs
+++ rust/kernel/firmware.rs
@@ -12,7 +12,11 @@ use core::ptr::NonNull;
 /// One of the following: `bindings::request_firmware`, `bindings::firmware_request_nowarn`,
 /// `bindings::firmware_request_platform`, `bindings::request_firmware_direct`.
 struct FwFunc(
-    unsafe extern "C" fn(*mut *const bindings::firmware, *const i8, *mut bindings::device) -> i32,
+    unsafe extern "C" fn(
+        *mut *const bindings::firmware,
+        *const ffi::c_char,
+        *mut bindings::device,
+    ) -> i32,
 );
 
 impl FwFunc {
