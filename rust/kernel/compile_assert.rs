6e25fc0246ea65eaed86d47c1afce04273c82c22
Christian Schrefl <chrisi.schrefl@gmail.com>
2025-06-07 21:44:38 +0200

    rust: add `assert_send` function
    Adds the `assert_send` function to `compile_assert.rs`.
    
    Signed-off-by: Christian Schrefl <chrisi.schrefl@gmail.com>

--- rust/kernel/compile_assert.rs
+++ rust/kernel/compile_assert.rs
@@ -22,3 +22,27 @@
 /// ```
 #[inline(always)]
 pub const fn assert_sync<T: ?Sized + Sync>() {}
+
+/// Asserts that the given type is [`Send`].
+///
+/// This check is done at compile time and does nothing at runtime.
+///
+/// # Examples
+///
+/// ```
+/// # use kernel::compile_assert::assert_send;
+/// # use core::cell::UnsafeCell;
+/// // Succeeds because `i32` is `Send`
+/// assert_send::<i32>();
+/// // Succeeds because `UnsafeCell<T>` is `Send` if `T: Send`
+/// assert_send::<UnsafeCell<()>>();
+/// ```
+///
+/// ```ignore # // TODO: This should be compile_fail once that works
+/// # use kernel::compile_assert::assert_send;
+/// # use kernel::NotThreadSafe;
+/// // Fails because `NotThreadSafe` is not `Send`.
+/// assert_send::<NotThreadSafe>();
+/// ```
+#[inline(always)]
+pub const fn assert_send<T: ?Sized + Send>() {}
